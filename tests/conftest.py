5d6a174cccba230e3e5d2638893611bbe57b6ba6
Christophe Salles <christophe.sallessacareau@gmail.com>
2026-01-05 15:51:40 +0100

    updeta de conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 # tests/conftest.py
-# briques de test réutilisables (fixtures)
+# briques de tests reutilisables (fixtures) pour tests sur l'API
+# TestClient de FastAPI pour simuler des requêtes HTTP
+# app est l'application FastAPI
 import pytest
 from fastapi.testclient import TestClient
+import sys 
+import os 
+sys.path.append(os.path.dirname(os.path.dirname(__file__))) # ajouter le repertoire parent au chemin d'importation
 from api import app
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
