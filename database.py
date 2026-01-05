4ad1dfb099681644484e102178abe858385b14eb
Christophe Salles <christophe.sallessacareau@gmail.com>
2026-01-05 18:37:00 +0100

    update de fichiers

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+# creation des tables
+# connexion base de donnees
 from sqlalchemy import create_engine, text
 from config import DB_URL
 
+# utilisation directe des credentials avec DB_URL
 engine = create_engine(DB_URL)
 
 def create_tables(schema_sql):
