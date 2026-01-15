f3b537cde29f6da7c5f952c6f3a31ea3a79de333
Christophe Salles <christophe.sallessacareau@gmail.com>
2026-01-15 20:08:35 +0100

    fichier csv fusionné et fichier test simplemodel

---
+++ tests/test_simplemodel.py
@@ -0,0 +1,29 @@
+# test unitaire 
+# test d'une prediction (N° de l'ID) quand tout est prêt
+
+import pandas as pd
+import pytest
+from unittest.mock import MagicMock, patch
+from model import SimpleModel
+
+
+@patch("model.joblib.load")
+@patch("model.pd.read_sql")
+def test_predict_one_ok(mock_read_sql, mock_load):
+    # faux modèle ML
+    fake_model = MagicMock()
+    fake_model.predict.return_value = [1]
+    mock_load.return_value = fake_model
+
+    # fausses données SQL
+    mock_read_sql.return_value = pd.DataFrame({
+        "id_employee": [3000],
+        "age": [30],
+        "salary": [50000],
+        "a_quitte_l_entreprise": [0]
+    })
+
+    model = SimpleModel("fake_model.joblib")
+    prediction = model.predict_one(1)
+
+    assert prediction == 1
