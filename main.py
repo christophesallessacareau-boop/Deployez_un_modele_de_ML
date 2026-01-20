a5407bb4f3e8d348b489258e0e7ffd495882516b
Christophe Salles <christophe.sallessacareau@gmail.com>
2026-01-20 11:03:09 +0100

    removes de fichiers devenus inutiles

--- main.py
+++
@@ -1,9 +0,0 @@
-# script de test rapide
-# test d'une prediction (N° de l'ID) quand tout est prêt
-# on evite de tout refaire (import, tables) à chaque test
-from model import SimpleModel
-
-model = SimpleModel("model.joblib")
-result = model.predict_one(10)
-print("Prediction :", result)
-
